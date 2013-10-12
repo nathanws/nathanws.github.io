f2f693ca895dd6f95f9f39effdb17e851a269abf
nathanws <nsllivn@gmail.com>
2013-10-12 11:40:43 -0500

    Added jquery. Tweaked side navigation.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var sideNavId = "#side-nav";
+
+function selectHomeNav () {
+	$(sideNavId + ' li.home').addClass('active');
+}
+
+function selectProjectsNav () {
+	$(sideNavId + ' li.projects').addClass('active');
+}
