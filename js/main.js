d44f365ef44345e76af2f76b69844bb281004c64
nathan <nathanws@users.noreply.github.com>
2018-10-18 14:26:05 -0500

    Switched to Hugo

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var sideNavId = "#side-nav";
-
-function selectHomeNav () {
-	$(sideNavId + ' li.home').addClass('active');
-}
-
-function selectProjectsNav () {
-	$(sideNavId + ' li.projects').addClass('active');
-}
